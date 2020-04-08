e7a702e0a74eda5e8b7e218a6d76528a68c468dd
nmscannell <scannell@uwm.edu>
2020-04-08 08:41:44 -0500

    working on web scraper to build dataset

---
+++ Classifier/news_data_scraper.py
@@ -0,0 +1,21 @@
+from newsapi import NewsApiClient
+
+# Init
+newsapi = NewsApiClient(api_key='92a066c49fa74acf9abfe52b04d5c7e4')
+
+# /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(q='coronavirus')['articles']
+
+# /v2/everything
+'''all_articles = newsapi.get_everything(q='bitcoin',
+                                      sources='bbc-news,the-verge',
+                                      domains='bbc.co.uk,techcrunch.com',
+                                      from_param='2017-12-01',
+                                      to='2017-12-12',
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page=2)
+
+# /v2/sources
+sources = newsapi.get_sources()
+'''
