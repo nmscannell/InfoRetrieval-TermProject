fe4535da1da718d73d79515e828e8b2ce1a2511e
nmscannell <scannell@uwm.edu>
2020-05-06 20:11:41 -0500

    updating ranking

--- QueryProcessing/QueryProcessor.py
+++ QueryProcessing/QueryProcessor.py
@@ -12,7 +12,6 @@ class QueryProcessor:
         self.query_string = query
         self.index = self.indexer.index
         self.source = source
-        print(source)
         self.parse_query()
 
     def parse_query(self):
@@ -36,26 +35,60 @@ class QueryProcessor:
             self.parse_query()
             return self.obtain_docs()
 
-        mes, docs = self.obtain_docs()
-        ranked = self.rank_docs(docs)
+        ranked = self.obtain_docs()
         if len(ranked) > 20:
             ranked = ranked[:20]
-        return mes, ranked
+        return ranked
 
-    def rank_docs(self, docs):
+    def rank_docs(self, common, docs):
+        # common is a list of common doc IDs
+        # docs is a list of lists of key-value pairs, doc ID-values for each term
         if len(self.index) == 0:
             raise Exception('No index has been made.')
         if len(docs) == 0:
             raise Exception('No documents to rank.')
-        doc_scores = [0] * len(docs)
-        # score depends on tf in title, tf in content, date if it exists****
-        # TODO: incorporate date and source/type
-        title_w = 0.5
+        doc_scores = [0] * len(common)
+        # score depends on tf in title, tf in content, type
+        title_w = 0.4
         content_w = 0.3
-        dates = [[i.docID, i.date] for i in docs]
-        dates.sort(reverse=True, key=lambda e: e[1])
+        source_w = 0.3
+
+        common_rank = []
+        for ids in common:
+            score = 0
+            for i in range(len(docs)):
+                idf = math.log(self.indexer.num_docs/len(docs[i]))
+                for pair in docs[i]:
+                    if pair[0] == ids:
+                        score += title_w * idf * pair[1]['title']
+                        score += content_w * idf * pair[1]['body']
+                        if pair[1]['source'] == 'experiment' or pair[1]['source'] == 'research':
+                            score += source_w
+                        elif pair[1]['source'] == 'medical':
+                            score += source_w * .75
+                        elif pair[1]['source'] == 'book':
+                            score += source_w * .5
+            common_rank.append((ids, score))
+
+        rank = []
+        for i in range(len(docs)):
+            idf = math.log(self.indexer.num_docs / len(docs[i]))
+            for pair in docs[i]:
+                if pair[0] not in common:
+                    score = 0
+                    score += title_w * idf * pair[1]['title']
+                    score += content_w * idf * pair[1]['body']
+                    if pair[1]['source'] == 'experiment' or pair[1]['source'] == 'research':
+                        score += source_w
+                    elif pair[1]['source'] == 'medical':
+                        score += source_w * .75
+                    elif pair[1]['source'] == 'book':
+                        score += source_w * .5
+                    rank.append((pair[0], score))
+
 
         # for each document, calculate the score based on tf-idf for each term in title and content
+
         for i in range(len(docs)):
             for j in self.query:
                 if docs[i].docID not in self.index[j]:
@@ -76,20 +109,50 @@ class QueryProcessor:
             raise Exception('Query terms not in index.')
         if len(self.index) == 0:
             raise Exception('No index has been made.')
+
+        bi_grams = []
+        prev = None
+        for i in self.query:
+            if prev is not None:
+                bi_grams.append(prev + ' ' + i)
+            prev = i
+
+        bi_post = []
+        bi_keys = []
+        for i in bi_grams:
+            if i in self.index:
+                p = []
+                keys = []
+                for k, v in self.index[i].items():
+                    keys.append(k)
+                    p.append((k, v))
+                bi_keys.append(keys)
+                bi_post.append(p)
+
+        # do something with bi_post
+        common_bi_docs = self.find_common_docs(bi_keys)
+
         postings = []
-        for i in range(len(self.query)):
-            postings.append(list(self.index[self.query[i]].keys()))
+        p_keys = []
+        for i in self.query:
+            if i in self.index:
+                p = []
+                keys = []
+                for k, v in self.index[i].items():
+                    keys.append(k)
+                    p.append((k, v))
+                postings.append(p)
+                p_keys.append(keys)
 
+        common_docs = self.find_common_docs(p_keys)
         # check what docs the terms have in common
-        common_docs = postings[0]
-        for i in range(len(common_docs)):
-            if i >= len(common_docs):
-                break
-            for j in range(1, len(postings)):
-                if common_docs[i] not in postings[j]:
-                    common_docs.remove(common_docs[i])
-                    break
 
+        bi_ranked = self.rank_docs(common_bi_docs, bi_post)
+        all_ranked = self.rank_docs(common_docs, postings)
+
+        # get ranked docs and return
+        docs = []
+        '''
         # if there are common terms, we will return results with both terms
         if len(common_docs) > 0:
             mes = 'Results contain all terms.'
@@ -108,4 +171,20 @@ class QueryProcessor:
             for i in postings:
                 for j in i:
                     docs.append(Document.objects.get(docID=j))
-        return mes, docs
+        '''
+        return docs
+
+    @staticmethod
+    def find_common_docs(postings):
+        common = postings[0]
+
+        for i in range(1, len(postings)):
+            if len(common) == 0:
+                break
+            for e in common:
+                if len(common) == 0:
+                    break
+                if e not in postings[i]:
+                    common.remove(e)
+
+        return common
--- medsearch/views.py
+++ medsearch/views.py
@@ -17,47 +17,47 @@ class ResultsView(View):
         query = str(request.POST["query"])
         source = str(request.POST['option'])
         proc = QueryProcessor(query, source)
-        mes, results = proc.perform_search()
+        results = proc.perform_search()
         return render(request, 'results.html', {"results": results})
 
 
 class RadiationView(View):
     def get(self, request):
         proc = QueryProcessor("radiation effects", 'all')
-        mes, results = proc.perform_search()
+        results = proc.perform_search()
         return render(request, 'results.html', {"results": results})
 
 
 class MicroView(View):
     def get(self, request):
         proc = QueryProcessor("microgravity", 'all')
-        mes, results = proc.perform_search()
+        results = proc.perform_search()
         return render(request, 'results.html', {"results": results})
 
 
 class VisionView(View):
     def get(self, request):
         proc = QueryProcessor("Vision Loss", 'all')
-        mes, results = proc.perform_search()
+        results = proc.perform_search()
         return render(request, 'results.html', {"results": results})
 
 
 class DystrophyView(View):
     def get(self, request):
         proc = QueryProcessor("muscular dystrophy", 'all')
-        mes, results = proc.perform_search()
+        results = proc.perform_search()
         return render(request, 'results.html', {"results": results})
 
 
 class IsolationView(View):
     def get(self, request):
         proc = QueryProcessor("psychological effects of isolation", 'all')
-        mes, results = proc.perform_search()
+        results = proc.perform_search()
         return render(request, 'results.html', {"results": results})
 
 
 class ReverseView(View):
     def get(self, request):
         proc = QueryProcessor("reverse blood flow", 'all')
-        mes, results = proc.perform_search()
+        results = proc.perform_search()
         return render(request, 'results.html', {"results": results})
